618aa098447982747054b2ac41e64cb668f7f33c
Justinsanjp <64624657+justinsanjp@users.noreply.github.com>
2024-06-23 02:47:14 +0200

    Create Deutsch.py
    German Language

---
+++ lang/Deutsch.py
@@ -0,0 +1,16 @@
+# Deutsch.py
+
+strings = {
+    "window_title": "YouTube Video Downloader",
+    "enter_url_label": "Gib den YouTube Video- oder Playlist-Link ein:",
+    "video_option": "Video",
+    "mp3_option": "MP3",
+    "highest_resolution": "Höchste Auflösung",
+    "lowest_resolution": "Niedrigste Auflösung",
+    "video_audio_separate": "Video und Audio (getrennt)",
+    "download_button": "Herunterladen",
+    "queue_label": "Download Warteschlange",
+    "completed_label": "Abgeschlossen",
+    "download_success": "erfolgreich heruntergeladen!",
+    "download_error": "Ein Fehler ist aufgetreten! bitte überprüfe die logs und öffne ein issue auf github, sollte der fehler erneurt auftreten. ",
+}
