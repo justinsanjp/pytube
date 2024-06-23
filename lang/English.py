5ac9eeead0ed450c07c3db973d1a065d4981c73d
Justinsanjp <64624657+justinsanjp@users.noreply.github.com>
2024-06-23 02:46:26 +0200

    Create English.py
    English language

---
+++ lang/English.py
@@ -0,0 +1,17 @@
+# English.py
+
+
+strings = {
+    "window_title": "YouTube Video Downloader",
+    "enter_url_label": "Enter a Youtube Video or Playlist link:",
+    "video_option": "Video",
+    "mp3_option": "MP3",
+    "highest_resolution": "Highest Resolution",
+    "lowest_resolution": "Lowest Resolution",
+    "video_audio_separate": "Video and Audio (spearated)",
+    "download_button": "Download",
+    "queue_label": "Download Queue",
+    "completed_label": "Completed",
+    "download_success": "Downloaded Succsessfully",
+    "download_error": "An error occurd. please retry. check error log & open an issue on github if this error returns.",
+}
